f45de900a3684e4c649b921d8176ace125e3b0fb
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-10-16 07:14:54 +0200

    finish documentation orientations
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>

--- src/orientation.rs
+++ src/orientation.rs
@@ -1,5 +1,5 @@
 /// Defines the default orientation for all the website's top level browsing
-/// contexts.
+/// contexts. [Read more.](https://www.w3.org/TR/screen-orientation)
 ///
 /// ## Example
 /// ```rust
@@ -15,28 +15,36 @@
 /// ```
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum Orientation {
-  ///
+  /// Enable `portrait-primary`, `portrait-secondary`, `landscape-primary` and
+  /// `landscape-secondary` orientations.
   #[serde(rename = "any")]
   Any,
-  ///
+  /// Enable `portrait-primary` or `landscape-primary` orientations such as the
+  /// associated current orientation angle is 0.
   #[serde(rename = "natural")]
   Natural,
-  ///
+  /// The screen width is greater than the screen height. This enables both
+  /// `landscape-primary` and `landscape-secondary` orientations.
   #[serde(rename = "landscape")]
   Landscape,
-  ///
+  /// The screen width is greater than the screen height, and there are multiple
+  /// orientations possible. This is the first of those possible orientations.
   #[serde(rename = "landscape-primary")]
   LandscapPrimary,
-  ///
+  /// The screen width is greater than the screen height, and there are multiple
+  /// orientations possible. This is the second of those possible orientations.
   #[serde(rename = "landscape-secondary")]
-  LandscapSecondary,
-  ///
+  LandscapeSecondary,
+  /// The screen height is greater than the screen width. This enables both
+  /// `portrait-primary` and `portrait-secondary` orientations.
   #[serde(rename = "portrait")]
   Portrait,
-  ///
+  /// The screen height is greater than the screen width, and there are multiple
+  /// orientations possible. This is the first of those possible orientations.
   #[serde(rename = "portrait-primary")]
   PortraitPrimary,
-  ///
+  /// The screen height is greater than the screen width, and there are multiple
+  /// orientations possible. This is the second of those possible orientations.
   #[serde(rename = "portrait-secondary")]
   PortraitSecondary,
 }
